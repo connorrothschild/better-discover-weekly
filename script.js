350b5d9ad1e7163793e6f399a221313913fbe7a1
Connor Rothschild <connorrothschild@gmail.com>
2020-06-16 11:35:17 -0400

    keys

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-var client_id = 'd3b460e727004b33957bde5c8802673c';
-var client_secret = '28e3103f948f4484911e8651580e7fe6';
-var redirect_uri = 'http://connorrothschild.github.io/better-discover-weekly/';
+// var client_id = 'd3b460e727004b33957bde5c8802673c';
+// var redirect_uri = 'http://connorrothschild.github.io/better-discover-weekly/';
